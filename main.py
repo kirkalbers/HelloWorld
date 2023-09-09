7e87263b2d8254712c710ba93be99f3c2de920ba
kirkalbers <kirkalbers@gmail.com>
2023-09-09 10:44:19 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from library import greet
+def main():
+    greet()
+
+
+if __name__ == '__main__':
+    main()
+
+# Hello, World!
+
+
